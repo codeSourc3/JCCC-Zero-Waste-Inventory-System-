56ddf549c364e86826b78389db8dcc3b154d3952
Enzo M <enzojmayo@gmail.com>
2021-05-10 20:53:44 -0500

    When updating new weight, does not update the current weight Fixes #63

--- services/bin-controller.js
+++ services/bin-controller.js
@@ -130,7 +130,7 @@ module.exports.editBin = async (req, res) => {
         if ('lastBinWeight' in requestBody) {
             // save current weight
             const bin = await binRepo.getById(Number(binId));
-            const lastWeight = new WeightHistory(1, Number(binId), bin.binWeight);
+            const lastWeight = new WeightHistory(1, Number(binId), requestBody.lastBinWeight);
             await weightRepo.add(lastWeight);
         }
         const index = await binRepo.update(Number(binId), requestBody);
